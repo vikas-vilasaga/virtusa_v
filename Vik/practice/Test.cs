7e38468b87219fcbe4c651a90ad7e0d00074451e
vikas-vilasaga <76689987+vikas-vilasaga@users.noreply.github.com>
2023-03-16 19:12:39 +0530

    Update Test.cs

--- Vik/practice/Test.cs
+++ Vik/practice/Test.cs
@@ -1,24 +1,24 @@
 
-Question:
-Minimum operations required to return to main folder
-John is working on a complex project. The main project directory 
-has many nested directories. I.e directory inside a directory.Duw to the complexity
-has to change the directory every time he has to work on a different modules.
-Sometimes he does not remember in which directory he is working and how
-to go back to the main directory.-root directory
+// Question:
+// Minimum operations required to return to main folder
+// John is working on a complex project. The main project directory 
+// has many nested directories. I.e directory inside a directory.Duw to the complexity
+// has to change the directory every time he has to work on a different modules.
+// Sometimes he does not remember in which directory he is working and how
+// to go back to the main directory.-root directory
 
-Write a program which would find the minimum steps required to go 
-back to the main directory from the current directory given the sequence of
-operations performed by join after entering the root directory.
-Jon can perform the following operations:
-1."../": Moves to the parent folder of the current folder. 
-(If current folder is the main it stays in the same folder)
-2."./":Remains in the same folder.
-3."F/": Moves to the child folder named F.
+// Write a program which would find the minimum steps required to go 
+// back to the main directory from the current directory given the sequence of
+// operations performed by join after entering the root directory.
+// Jon can perform the following operations:
+// 1."../": Moves to the parent folder of the current folder. 
+// (If current folder is the main it stays in the same folder)
+// 2."./":Remains in the same folder.
+// 3."F/": Moves to the child folder named F.
 
-Input 1- 6
-Input 2- arr[] = {"F1/","F2/","./","F3/","../","F31/"}
-Output - 3
+// Input 1- 6
+// Input 2- arr[] = {"F1/","F2/","./","F3/","../","F31/"}
+// Output - 3
 
 using System;
 
